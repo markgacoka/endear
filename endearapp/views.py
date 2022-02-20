219164024df872a6a06ddde6b7d991685de19d14
Mark Gacoka Mbui <markgacoka@gmail.com>
2022-02-20 08:22:17 +0000

    Add crush to DB

--- endearapp/views.py
+++ endearapp/views.py
@@ -1,4 +1,7 @@
+import datetime
 from django.shortcuts import render
+from django.http import JsonResponse
+from endearapp.models import CrushModel
 
 def home(request):
     context = {}
@@ -7,3 +10,15 @@ def home(request):
 def dashboard(request):
     context = {}
     return render(request, 'dashboard.html', context)
+
+def add_crush(request):
+    if request.method == 'POST':
+        response_data = {"name": request.POST.get('name'), "email": request.POST.get('email')}
+        crush_model = CrushModel.objects.create(
+            original_user = request.user,
+            crush_name = response_data['name'],
+            crush_email = response_data['email'],
+            record_date = datetime.datetime.now())
+        
+        crush_model.save()
+    return JsonResponse(response_data, status=201)
