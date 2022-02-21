19b18d1c04f8e33f1a479997eb231187be79b0a0
Mark Gacoka Mbui <markgacoka@gmail.com>
2022-02-21 06:23:03 +0000

    Fix alignment bug in crush list

--- endearproject/settings.py
+++ endearproject/settings.py
@@ -69,9 +69,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'libraries': {
-                'custom_tags':'endearapp.template_tags.custom_tags'
-            },
         },
     },
 ]
