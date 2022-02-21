63150e36cb29cb99c665f4c6b7c8ce773e2009a1
Mark Gacoka Mbui <markgacoka@gmail.com>
2022-02-21 13:49:13 +0000

    Fix the empty crush list bug on adding more than three matches

--- endearapp/views.py
+++ endearapp/views.py
@@ -10,12 +10,15 @@ def home(request):
 def dashboard(request):
     context, crushes = {}, {}
     user_crushes = CrushModel.objects.filter(original_user=request.user).values('crush_name','crush_email')
+    for crush in user_crushes:
+        crushes[crush['crush_name']] = crushes.get(crush['crush_name'], crush['crush_email'])
     if request.method == 'POST':
-        print(request.POST)
         if 'delete' in request.POST:
             CrushModel.objects.filter(crush_email__iexact=request.POST.get('delete')).delete()
             return redirect('/dashboard/')
         elif 'name' in request.POST and 'email' in request.POST:
+            if request.POST.get('name') in crushes:
+                pass
             if len(user_crushes) < 3: 
                 response_data = {"name": request.POST.get('name'), "email": request.POST.get('email')}
                 crush_model = CrushModel.objects.create(
@@ -26,11 +29,10 @@ def dashboard(request):
                 crush_model.save()
                 return redirect('/dashboard/')
             else:
-                print('Number of crushes exceeded!')
+                context['crushes'] = crushes
+                return render(request, 'dashboard.html', context)
         else:
             return render(request, 'dashboard.html', context)
     else:
-        for crush in user_crushes:
-            crushes[crush['crush_name']] = crushes.get(crush['crush_name'], crush['crush_email'])
         context['crushes'] = crushes
     return render(request, 'dashboard.html', context)
