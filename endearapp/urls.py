6b429d81801b5558fd57372face30a9856d2f3d1
Mark Gacoka Mbui <markgacoka@gmail.com>
2022-02-28 12:12:31 +0000

    My blob on the morality of wat

--- endearapp/urls.py
+++ endearapp/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('morality-of-war/', views.war, name='war'),
     path('solution/', views.solution, name='solution'),
     path('error/', views.login_error, name='login_error'),
 ]
